97f84af5e85d617d0a0a08f588fc04de0d7f3296
File_Missing <michaverheij@gmail.com>
2025-09-24 15:13:59 +0200

    added mising items, balanced item costs, fixed item usage bug

--- Assets/Scripts/Combat/Item.cs
+++ Assets/Scripts/Combat/Item.cs
@@ -3,36 +3,50 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "Combat/Item")]
 public class Item : ScriptableObject
 {
+    [Header("Info")]
     new public string name;
     public string description;
     public string details;
     public Sprite icon;
     public int cost;
 
+    [Header("Combat Stats")]
     [Tooltip("if false, will remove effect instead of adding")]
     public bool givesStatusEffect;
-    public StatusEffectType statusEffectType; 
+    public int appliedLevel;
+    public StatusEffectType statusEffectType;
+    public int healing;
 
     public void Use(Combatant user, Combatant target)
     {
-        StatusEffect statusEffect = statusEffectType switch
+        if (statusEffectType != StatusEffectType.None)
         {
-            StatusEffectType.Bleed => new BleedEffect(1),
-            StatusEffectType.Poison => new PoisonEffect(1),
-            StatusEffectType.Entangled => new EntangledEffect(1),
-            StatusEffectType.Confused => new ConfusedEffect(1),
-            StatusEffectType.Wet => new WetEffect(1),
-            StatusEffectType.SkillIssued => new SkillIssuedEffect(1),
-            _ => null,
-        };
+            StatusEffect statusEffect = statusEffectType switch
+            {
+                StatusEffectType.Bleed => new BleedEffect(1),
+                StatusEffectType.Poison => new PoisonEffect(1),
+                StatusEffectType.Entangled => new EntangledEffect(1),
+                StatusEffectType.Confused => new ConfusedEffect(1),
+                StatusEffectType.Wet => new WetEffect(1),
+                StatusEffectType.SkillIssued => new SkillIssuedEffect(1),
+                _ => null,
+            };
 
-        if (givesStatusEffect)
-        {
-            statusEffect.ApplyEffect(target);
+            statusEffect.level = appliedLevel;
+
+            if (givesStatusEffect)
+            {
+                statusEffect.ApplyEffect(target);
+            }
+            else
+            {
+                statusEffect.RemoveEffect(user);
+            } 
         }
-        else
+
+        if (healing != 0)
         {
-            statusEffect.RemoveEffect(target);
+            user.Heal(healing);
         }
 
         CombatManager.instance.playerCombat.playerStats.items.Remove(this);
